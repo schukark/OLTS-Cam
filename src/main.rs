e76e49ebd9865581952442a5cf7aab3d21b7e105
schukark <schukark2004@gmail.com>
2024-12-09 18:16:58 +0300

    added: (structure) basic commands according to needs of the project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-use teloxide::prelude::*;
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[derive(Clone, BotCommands)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "Display current objects")]
+    CurrentState,
+
+    #[command(description = "Search for object")]
+    WhereIs(String),
+
+    #[command(description = "Change settings to the camera")]
+    SettingsCamera(String),
+
+    #[command(description = "Change settings to the filesystem")]
+    SettingsFilesystem(String),
+
+    #[command(description = "Change settings to the CV model")]
+    SettingsCVModel(String),
+}
 
 #[tokio::main]
 async fn main() {
@@ -7,9 +29,50 @@ async fn main() {
 
     let bot = Bot::from_env();
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    Command::repl(bot, answer).await;
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::CurrentState => {
+            bot.send_message(msg.chat.id, String::from("current-state"))
+                .await?
+        }
+        Command::WhereIs(object_name) => {
+            bot.send_message(msg.chat.id, object_name.to_string())
+                .await?
+        }
+        Command::SettingsCamera(options) => {
+            bot.send_message(
+                msg.chat.id,
+                format!(
+                    "The settings options for the camera are: {}",
+                    options.split_whitespace().collect::<Vec<_>>().join("; ")
+                ),
+            )
+            .await?
+        }
+        Command::SettingsFilesystem(options) => {
+            bot.send_message(
+                msg.chat.id,
+                format!(
+                    "The settings options for the filesystem are: {}",
+                    options.split_whitespace().collect::<Vec<_>>().join("; ")
+                ),
+            )
+            .await?
+        }
+        Command::SettingsCVModel(options) => {
+            bot.send_message(
+                msg.chat.id,
+                format!(
+                    "The settings options for the cvmodel are: {}",
+                    options.split_whitespace().collect::<Vec<_>>().join("; ")
+                ),
+            )
+            .await?
+        }
+    };
+
+    Ok(())
 }
