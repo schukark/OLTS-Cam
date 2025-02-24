6df455f541a9c9912a1680281b4c96b64cfc97e1
schukark <schukark2004@gmail.com>
2025-02-24 16:01:07 +0300

    added: (modules) requests module with serde_json info

---
+++ src/requests.rs
@@ -0,0 +1,24 @@
+// use base64::{engine::general_purpose::STANDARD, Engine as _};
+use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+
+#[derive(Debug, Deserialize)]
+pub struct RequestData {
+    key: String,
+    // Add more fields as required
+}
+
+#[derive(Debug, Serialize)]
+pub struct ResponseData {
+    message: String,
+}
+
+pub async fn handle_request(data: RequestData) -> Result<impl warp::Reply, Infallible> {
+    println!("Received JSON: {:?}", data);
+
+    let response = ResponseData {
+        message: format!("Received key: {}", data.key),
+    };
+
+    Ok(warp::reply::json(&response))
+}
