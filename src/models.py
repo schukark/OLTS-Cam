870942aca95fa059a71152b2f67738a5d5bf9230
schukark <schukark2004@gmail.com>
2025-02-25 15:16:13 +0300

    added: (python-server) models openapi

---
+++ src/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import List
+
+
+class SettingsInner(BaseModel):
+    key: str
+    value: str
+
+
+class Receiver(str, Enum):
+    Camera = "camera"
+    Db = "db"
+    Fs = "fs"
+
+
+class Settings(BaseModel):
+    receiver: Receiver
+    settings: List[SettingsInner]
+
+class ObjectPhoto(BaseModel):
+    height: int
+    width: int
+    image: str
