52cf83314a6be2aa8f65f6758f19397b420244ac
schukark <schukark2004@gmail.com>
2025-05-04 18:22:54 +0300

    fix: server now responds and reads settings/image files correctly

--- src/server.py
+++ src/server.py
@@ -1,36 +1,78 @@
-from fastapi import FastAPI
+import json
+from typing import Any, Dict, List, Optional
+from fastapi import FastAPI, Response
 from models import Settings, ObjectPhoto, Receiver
 from pydantic_core import from_json
 import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/settings/{rcv}")
-async def get_settings(rcv: Receiver):
+async def get_settings(rcv: Receiver, response: Response) \
+        -> Optional[Settings]:
     with open("settings.json", "r") as settings_file:
-        settings = from_json("\n".join(settings_file.readlines()))
-    return settings
+        settings: Dict[str, List[Any]] = from_json(
+            "\n".join(settings_file.readlines()))
+    result = list(filter(lambda x: check_rcv_type(x, rcv),
+                         settings["settings"]))
+
+    if len(result) == 0:
+        response.status_code = 422
+        return None
+
+    return result[0]
+
+
+def check_rcv_type(x: Dict[str, Any], rcv: Receiver) -> bool:
+    return x["receiver"] == rcv
 
 
 @app.get("/object/{name}")
 async def get_object(name: str):
-    return ObjectPhoto(height=224, width=224, image="iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=")
+    with open("test_image.txt", "r") as file_contents:
+        test_image = file_contents.readline()
+    return ObjectPhoto(height=224, width=224, image=test_image)
+
+
+@app.get("/objects/")
+async def get_objects():
+    with open("test_image2.txt", "r") as file_contents:
+        test_image = file_contents.readline()
+    return ObjectPhoto(height=224, width=224, image=test_image)
 
 
 @app.post("/settings/")
-async def change_settings(new_settings: Settings):
+async def change_settings(new_settings: Settings, response: Response):
     with open("settings.json", "r") as settings_file:
-        cur_settings = from_json("\n".join(settings_file.readlines())).dict()
+        cur_settings = from_json(
+            "\n".join(settings_file.readlines()))
 
-    for i in range(len(cur_settings["settings"])):
-        rcv_settings: Settings = cur_settings["settings"][i]
+    found_rcv = False
 
-        if rcv_settings.receiver != new_settings.receiver:
+    for json_row in cur_settings["settings"]:
+        if json_row["receiver"] != new_settings.receiver:
             continue
 
-        cur_settings["settings"][i] = new_settings.settings
+        found_rcv = True
+        for set_row in new_settings.settings:
+            key = set_row.key
+            value = set_row.value
+
+            if key not in list(map(lambda x: x["key"], json_row["settings"])):
+                response.status_code = 401
+                return
+
+            for row in json_row["settings"]:
+                if row["key"] == key:
+                    row["value"] = value
+                    break
+
+    if not found_rcv:
+        response.status_code = 422
+        return
 
     with open("settings.json", "w") as settings_file:
-        settings_file.write(Settings(**cur_settings).model_dump_json)
+        settings_file.write(json.dumps(cur_settings))
 
 uvicorn.run(app, host="127.0.0.1", port=19841)
