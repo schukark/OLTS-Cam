2d61a85cc0e59319fbfc83a35c56828218361a8d
schukark <schukark2004@gmail.com>
2025-05-05 15:44:36 +0300

    feat: logging into a file instead of to stdout

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@
 //! your belongings at home through a camera and
 //! query a telegram bot about it
 
+use std::fs::File;
+
+use anyhow::Result;
+use chrono::Local;
 use dotenv::dotenv;
 use futures::future::join_all;
+use std::io::Write;
 
 mod api_client;
 mod bot;
@@ -18,12 +23,30 @@ mod models;
 /// The main function that runs the bot
 #[tokio::main]
 #[cfg_attr(coverage_nightly, coverage(off))]
-pub async fn main() {
+pub async fn main() -> Result<()> {
     dotenv().ok();
 
-    pretty_env_logger::init();
+    let target = Box::new(File::create("log.txt").expect("Can't create file"));
+
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Info)
+        .target(env_logger::Target::Pipe(target))
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "[{} {} {}:{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                record.args()
+            )
+        })
+        .init();
 
     let bot_task = tokio::spawn(bot::run_bot());
 
     join_all(vec![bot_task]).await;
+
+    Ok(())
 }
